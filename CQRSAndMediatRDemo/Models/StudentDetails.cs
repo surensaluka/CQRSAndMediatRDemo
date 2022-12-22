cff2cfc750eca3b3780ead9167452d2e5d32e2e0
Suren Manawatta <surenm@boardpac.co>
2022-12-22 18:56:42 +0530

    Add project files.

---
+++ CQRSAndMediatRDemo/Models/StudentDetails.cs
@@ -0,0 +1,11 @@
+﻿namespace CQRSAndMediatRDemo.Models
+{
+    public class StudentDetails
+    {
+        public int Id { get; set; }
+        public string StudentName { get; set; } = string.Empty;
+        public string StudentEmail { get; set; } = string.Empty;
+        public string StudentAddress { get; set; } = string.Empty;
+        public int StudentAge { get; set; }
+    }
+}
---
+++ CQRSAndMediatRDemo/Repositories/StudentRepository.cs
@@ -0,0 +1,51 @@
+﻿using CQRSAndMediatRDemo.Data;
+using CQRSAndMediatRDemo.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+namespace CQRSAndMediatRDemo.Repositories
+{
+    public class StudentRepository : IStudentRepository
+    {
+        private readonly DbContextClass _dbContext;
+
+        public StudentRepository(DbContextClass dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<StudentDetails> AddStudentAsync(StudentDetails studentDetails)
+        {
+            var result = await _dbContext.Students.AddAsync(studentDetails);
+            await _dbContext.SaveChangesAsync();
+            return result.Entity;
+        }
+
+        public async Task<int> DeleteStudentAsync(int Id)
+        {
+            var student = await _dbContext.Students.FirstOrDefaultAsync(x => x.Id == Id);
+            if (student == null) throw new ArgumentException(HttpStatusCode.NotFound.ToString());
+
+            _dbContext.Students.Remove(student);
+            return await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<StudentDetails> GetStudentByIdAsync(int Id)
+        {
+            var student = await _dbContext.Students.FirstOrDefaultAsync(x => x.Id == Id);
+            if (student == null) throw new ArgumentException(HttpStatusCode.NotFound.ToString());
+            return student;
+        }
+
+        public async Task<List<StudentDetails>> GetStudentListAsync()
+        {
+            return await _dbContext.Students.ToListAsync();
+        }
+
+        public async Task<int> UpdateStudentAsync(StudentDetails studentDetails)
+        {
+            _dbContext.Students.Update(studentDetails);
+            return await _dbContext.SaveChangesAsync();
+        }
+    }
+}
